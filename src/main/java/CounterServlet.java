ddf1f4d336eae4aa3e5cb4792f6a7b24b1137f0d
M Coindreau <mariannacoindreau@gmail.com>
2017-10-17 15:37:43 -0500

    Servlets exercises completed

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+    private int counter = 0;
+
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws ServletException, IOException {
+        if (request.getParameter("reset") != null) {
+            counter = 0;
+        }
+
+        counter++;
+
+        response.getWriter().print(
+                "<h1>You have visited this page " + counter + " times</h1>"
+        );
+    }
+}
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -7,9 +7,21 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello World!</h1>");
+    protected void doGet(
+            HttpServletRequest request, // input
+            HttpServletResponse response // output
+    ) throws IOException{
+//        ****PRINTS STATIC RESPONSE****
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//        out.println("<h1>Hello World!</h1>");
+        String name = request.getParameter("name");
+
+//        if name is given
+        if (name == null || name.trim().isEmpty()){
+            response.getWriter().print("<h1>Hello World!</h1>");
+        } else {
+            response.getWriter().print("<h1>Hello " + name + "!</h2>");
+        }
     }
 }
