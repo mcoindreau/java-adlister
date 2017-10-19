74a49b3f516c9d68eea8414e88ffecc9db127664
M Coindreau <mariannacoindreau@gmail.com>
2017-10-19 17:30:42 -0500

    Session and cookies completed

---
+++ src/main/java/user.java
@@ -0,0 +1,16 @@
+import java.io.Serializable;
+
+public class user implements Serializable{
+
+    private long id;
+    private long name;
+    private String email;
+    private String password;
+
+//    public Ad(long id, long userId, String title, String description) {
+//        this.id = id;
+//        this.userId = userId;
+//        this.title = title;
+//        this.description = description;
+//    }
+}
