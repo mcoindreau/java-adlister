35bf46dba6c4443810d5cc5b7c0d6236494b2db1
M Coindreau <mariannacoindreau@gmail.com>
2017-10-24 10:06:36 -0500

    User input exercises completed

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,19 +1,43 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO: show the registration form
+        request.getRequestDispatcher("/WEB-INF/register.jsp")
+        .forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
         // TODO: ensure the submitted information is valid
-        // TODO: create a new user based off of the submitted information
+
+        if(username != null && email != null && password != null && !username.isEmpty() && !email.isEmpty() &&!password.isEmpty()) {
+
+
+        // TODO: create a new user based off of the submitted
+            User user = new User (0, username, email, password);
+
+
         // TODO: if a user was successfully created, send them to their profile
+            DaoFactory.getUsersDao().insert(user);
+            request.getSession().setAttribute("user", user); //automatic login
+            response.sendRedirect("/profile");
+        }else {
+            doGet(request, response);
+        }
+
     }
 }
---
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -0,0 +1,52 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+
+public class MySQLUsersDao implements Users{
+
+    private Connection connection = null;
+
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+
+    public Long insert(User user) {
+        try {
+            // ****** BEFORE *****
+            // Statement stmt = connection.createStatement();
+            // stmt.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+            String sql = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+            PreparedStatement stmt = connection.prepareStatement(
+                    sql,                                // here the second argument is
+                    Statement.RETURN_GENERATED_KEYS     // only necessary for an insert query
+            );
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getPassword());
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new ad.", e);
+        }
+    }
+}
